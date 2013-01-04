5569d708a864dd2ad4369a7e4ab04a9276e2f957
jhemsley30 <jhemsley30@yahoo.com>
2013-01-04 09:50:52 -0800

    styles

--- sites/all/themes/zen/ashly/templates/page--node--10.tpl.php
+++ sites/all/themes/zen/ashly/templates/page--node--10.tpl.php
@@ -112,22 +112,17 @@
 				<a class="fancybox"  rel="group" href="#inline">
 	login
 </a>
-	<div id='inline' style='display:none;'>
+	  <div id='inline' style='display:none;'>
 	<h1 class='loginTitle'>Red Truck Inc.</h1>
 	
 	 <form action="user/login" method="post" id="user-login" accept-charset="UTF-8"><div><div  class="form-item form-type-textfield form-item-name" > 
-  <label for="edit-name">Username <span class="form-required" title="This field is required.">*</span></label>
- <input type="text" id="edit-name" name="name" value="User Name" size="50" maxlength="128" class="form-text topText required rounded" onFocus="this.value=' '" />
+  <label for="edit-name">&nbsp;<span class="form-required" title="This field is required."></span></label>
+ <input type="text" id="edit-name" name="name" value="" size="50" maxlength="128" class="form-text required rounded" />
 <div class="description"><!-- Enter your Theme Exchange username. -->&nbsp;</div>
 </div> <!-- End ashContent -->
 <div class="form-item form-type-password form-item-pass">
-
-
-  <label for="edit-pass">Password <span class="form-required" title="This field is required.">*</span></label>
- <input type="text" id="edit-pass" name="pass" size="50" maxlength="128" class="form-text required rounded"  value="Password"
-    onblur="this.value=!this.value?'Password':this.value;"
-    onfocus="this.select()"
-    onclick=" if (this.value=='Password'){this.value=''; this.type='password'}"/>
+  <label for="edit-pass">&nbsp;<span class="form-required" title="This field is required."></span></label>
+ <input type="password" id="edit-pass" name="pass" size="50" maxlength="128" class="form-text required rounded" />
 <div class="description"><!-- Enter the password that accompanies your username. -->&nbsp;</div>
 </div>
 <input type="hidden" name="form_build_id" value="form-I80-7cjV7RBQ7eO1bxGywGBbMGLuh0TbaK2PBKHpHC4" />
@@ -135,7 +130,8 @@
 <div class="form-actions form-wrapper" id="edit-actions">
 <input type="submit" id="edit-submit" name="op" value="Log in" class="form-submit" /></div></div></form>
 	
-	</div><!-- end inline -->
+	</div><!-- -------------------------------------end inline -------------------------------------------------------------- -->
+
 				</p>
      </div><!-- END footerInside -->
 
