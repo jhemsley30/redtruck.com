b6ca6c90af608a314075296cfbdb9c633faf350d
jhemsley30 <jhemsley30@yahoo.com>
2013-01-21 17:49:52 -0800

    changed views function

--- sites/all/themes/zen/ashly/ADD_THIS_FUNCTION_ON_VIEWS_Update-views_handler_field.pas
+++ sites/all/themes/zen/ashly/ADD_THIS_FUNCTION_ON_VIEWS_Update-views_handler_field.pas
@@ -3,6 +3,10 @@
   /** ##########################################################################################################################
       JH- THIS WAS ADDED TO ALLOW 'style="" into rewrite section of views, make sure to replace in updated modules of views
 	  
+	  look in sites/all/modules/contrib/views/handlers/views_handler_field
+	  
+	  it will be added at the very bottom of the page
+	  
 	  REPLACE THIS FUNCTION IN THE NEW VIEWS WITH THIS FUNCTION
 	  ########################################################################################################################## **/
   function render_altered($alter, $tokens) {     
